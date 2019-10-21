75f4d4ffc55109453f1541bff0d3276d167b3d8b
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-21 11:11:01 +0300

    init

--- CustomCellInList/ContentView.swift
+++ CustomCellInList/ContentView.swift
@@ -10,12 +10,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        ScrollView(.vertical, showsIndicators: true) {
+            VStack{
+                ForEach(0..<50) {_ in
+                    Custom()
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct Custom: View {
+    var body: some View {
+        HStack {
+            Button(action: {
+                print("botton 1")
+            }) {
+                Text("Button 1")
+            }.frame(width: 100, height: 50)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .cornerRadius(25)
+            .padding()
+            
+            Button(action: {
+                print("botton 2")
+            }) {
+                Text("Button 2")
+            }.frame(width: 100, height: 50)
+            .background(Color.green)
+            .foregroundColor(.white)
+            .cornerRadius(25)
+
+            Button(action: {
+                print("botton 3")
+            }) {
+                Text("Button 3")
+            }.frame(width: 100, height: 50)
+            .background(Color.orange)
+            .foregroundColor(.white)
+            .cornerRadius(25)
+            .padding()
+        }
     }
 }
